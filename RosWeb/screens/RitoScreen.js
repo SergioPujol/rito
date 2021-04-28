96b2f2b0542bff6e13107dcf7b3b8610ba5f31fe
fbs11 <46707393+fbs11@users.noreply.github.com>
2021-04-28 20:19:40 +0200

    R-H2-1 mapeado y zonas actualizadas

--- RosWeb/screens/RitoScreen.js
+++ RosWeb/screens/RitoScreen.js
@@ -1,6 +1,14 @@
 import React, { useState, useEffect, useRef } from "react";
 import { ImageBackground, View, Text, TouchableOpacity, Button, StyleSheet, Image, CheckBox, Modal } from 'react-native';
 import Animated from 'react-native-reanimated';
+import { Dimensions } from 'react-native';
+
+let screenWidth = Dimensions.get('window').width;
+let screenHeight = Dimensions.get('window').height;
+screenHeight = screenHeight - (screenHeight * 0.14)
+
+const mapLength = { x: 9.2, y: 9.2 }
+
 
 const bgImg = require('../assets/img/map.png')
 function onPress(location) {
@@ -48,123 +56,186 @@ const ModalPop = ({ visible, children }) => {
 
 
 const RitoScreen = ({ navigation }) => {
+
+    const [robotY, setRobotY] = React.useState(screenHeight);
+    const [robotX, setRobotX] = React.useState((screenWidth / 2) - 20);
+    //console.log(robotX)
+
+    function cambiarCoordenadas(coordGazebo) {
+        console.log(coordGazebo)
+        let nx = (screenWidth / 2) - 20 + (screenWidth - 20) * (coordGazebo.y / mapLength.y);
+        let ny = screenHeight - screenHeight * (coordGazebo.x / mapLength.x * (-1));
+
+
+        let coordCambiadas = { x: nx, y: ny }
+
+        return coordCambiadas;
+
+    }
+    //Aquí irá la lectura del topic
+    function x() {
+        let coordenadasGazeboInput = { x: -7, y: -4 }
+        let coordReales = cambiarCoordenadas(coordenadasGazeboInput)
+        setRobotX(coordReales.x)
+        setRobotY(coordReales.y)
+    }
+
     const [visible, setVisible] = React.useState(false);
     const [isSelected, setSelection] = React.useState(false);
     const [isSelected1, setSelection1] = React.useState(false);
     const [isSelected2, setSelection2] = React.useState(false);
     return (
         <View style={styles.container}>
-            <View style={styles.mapContainer}>
-                <ImageBackground source={bgImg} style={styles.image}>
-                    <View style={styles.topBtnContainer}>
-                        <TouchableOpacity
-                            style={styles.btnTop}
-                            onPress={() => onPress("pescaderia")}
-                        >
-                        </TouchableOpacity>
 
-                        <TouchableOpacity
-                            style={styles.btnTop}
-                            onPress={() => onPress("charcuteria")}
-                        >
-                        </TouchableOpacity>
-                    </View>
-                    <View style={styles.botBtnContainer}>
-                        <TouchableOpacity
-                            style={styles.btnBot}
-                            onPress={() => onPress("caja")}
-                        >
-                        </TouchableOpacity>
-                    </View>
-                    <View>
-                        <TouchableOpacity style={{
-                            backgroundColor: colors.primary,
-                            width: 70,
-                            height: 70,
-                            borderRadius: 35,
-                            justifyContent: 'center',
+            <ImageBackground source={bgImg} style={styles.image}>
+                <View style={{
+                    backgroundColor: '#00B185',
+                    width: 40,
+                    height: 40,
+                    borderRadius: 100,
+                    position: 'absolute',
+                    top: robotY,
+                    left: robotX
+                }}
+                ></View>
+                <View style={styles.topBtnContainer}>
+                    <TouchableOpacity
+                        style={{
+                            ...styles.btnTop, justifyContent: 'center',
                             alignItems: 'center',
-                            bottom: -40,
-                            right: -15,
-                            ...style.shadow
-                        }} onPress={() => setVisible(true)}>
-                            <Image source={require('../assets/icons/navigator.png')}
-                                resizeMode='contain'
+                        }}
+                        onPress={() => { onPress("pescaderia"); x() }}
+                    >
+                        <Image source={require('../assets/icons/pescaderia.png')}
+                            style={{
+                                ...styles.btnTop,
+                                width: 64,
+                                height: 64,
+
+                            }}></Image>
+                    </TouchableOpacity>
+
+                    <TouchableOpacity
+                        style={{
+                            ...styles.btnTop, justifyContent: 'center',
+                            alignItems: 'center',
+                        }}
+                        onPress={() => onPress("charcuteria")}
+                    >
+                        <Image source={require('../assets/icons/carne.png')}
+                            style={{
+                                ...styles.btnTop,
+                                width: 64,
+                                height: 64,
+
+                            }}></Image>
+                    </TouchableOpacity>
+                </View>
+                <View style={styles.botBtnContainer}>
+                    <TouchableOpacity style={{
+                        backgroundColor: colors.primary,
+                        width: 70,
+                        height: 70,
+                        borderRadius: 35,
+                        justifyContent: 'center',
+                        alignItems: 'center',
+                        bottom: -40,
+                        right: -15,
+                        ...style.shadow
+                    }} onPress={() => setVisible(true)}>
+                        <Image source={require('../assets/icons/navigator.png')}
+                            resizeMode='contain'
+                            style={{
+                                width: 48,
+                                height: 48,
+                                tintColor: colors.white,
+                            }}></Image>
+                    </TouchableOpacity>
+                    <TouchableOpacity
+                        style={{
+                            ...styles.btnTop, justifyContent: 'center',
+                            alignItems: 'center',
+                        }}
+                        onPress={() => onPress("cajas")}
+                    >
+                        <Image source={require('../assets/icons/caja_registradora.png')}
+                            style={{
+                                ...styles.btnTop,
+                                width: 84,
+                                height: 84,
+
+                            }}></Image>
+                    </TouchableOpacity>
+
+                </View>
+                <View>
+                    <ModalPop visible={visible}>
+                        <View style={{ alignItems: 'center' }}>
+                            <View style={styles.header}>
+                                <TouchableOpacity onPress={() => setVisible(false)}>
+                                    <Image source={require("../assets/icons/cancel.png")} style={{ height: 20, width: 20, alignSelf: 'flex-end' }}></Image>
+                                </TouchableOpacity>
+                                <Text style={styles.headerText}>Selecciona la ruta</Text>
+                            </View>
+                        </View>
+                        <View style={styles.checkboxContainer}>
+                            <View style={styles.checkboxComponent}>
+                                <CheckBox
+                                    value={isSelected}
+                                    onValueChange={setSelection}
+                                    style={styles.checkbox}
+                                />
+                                <Text style={styles.label}>Pescaderia</Text>
+                            </View>
+                            <View style={styles.checkboxComponent} value={isSelected1} onValueChange={setSelection1}>
+                                <CheckBox
+                                    value={isSelected1}
+                                    onValueChange={setSelection1}
+                                    style={styles.checkbox}
+                                />
+                                <Text style={styles.label}>Charcuteria</Text>
+                            </View>
+                            <View style={styles.checkboxComponent}>
+                                <CheckBox
+                                    value={isSelected2}
+                                    onValueChange={setSelection2}
+                                    style={styles.checkbox}
+                                />
+                                <Text style={styles.label}>Cajas</Text>
+                            </View>
+                        </View>
+                        <View style={{ alignItems: 'center', flexDirection: 'row', justifyContent: 'space-evenly' }}>
+                            <TouchableOpacity style={{
+                                backgroundColor: colors.secondary,
+                                width: 70,
+                                height: 40,
+                                borderRadius: 5,
+                                justifyContent: 'center',
+                                alignItems: 'center',
+                                ...style.shadow
+                            }}>
+                                <Text>MOVER</Text>
+                            </TouchableOpacity>
+                            <TouchableOpacity
                                 style={{
-                                    width: 48,
-                                    height: 48,
-                                    tintColor: colors.white,
-                                }}></Image>
-                        </TouchableOpacity>
-                    </View>
-                    <View>
-                        <ModalPop visible={visible}>
-                            <View style={{ alignItems: 'center' }}>
-                                <View style={styles.header}>
-                                    <TouchableOpacity onPress={() => setVisible(false)}>
-                                        <Image source={require("../assets/icons/cancel.png")} style={{ height: 20, width: 20, alignSelf: 'flex-end' }}></Image>
-                                    </TouchableOpacity>
-                                    <Text style={styles.headerText}>Selecciona la ruta</Text>
-                                </View>
-                            </View>
-                            <View style={styles.checkboxContainer}>
-                                <View style={styles.checkboxComponent}>
-                                    <CheckBox
-                                        value={isSelected}
-                                        onValueChange={setSelection}
-                                        style={styles.checkbox}
-                                    />
-                                    <Text style={styles.label}>Pescaderia</Text>
-                                </View>
-                                <View style={styles.checkboxComponent} value={isSelected1} onValueChange={setSelection1}>
-                                    <CheckBox
-                                        value={isSelected1}
-                                        onValueChange={setSelection1}
-                                        style={styles.checkbox}
-                                    />
-                                    <Text style={styles.label}>Charcuteria</Text>
-                                </View>
-                                <View style={styles.checkboxComponent}>
-                                    <CheckBox
-                                        value={isSelected2}
-                                        onValueChange={setSelection2}
-                                        style={styles.checkbox}
-                                    />
-                                    <Text style={styles.label}>Cajas</Text>
-                                </View>
-                            </View>
-                            <View style={{ alignItems: 'center', flexDirection: 'row', justifyContent: 'space-evenly' }}>
-                                <TouchableOpacity style={{
                                     backgroundColor: colors.secondary,
                                     width: 70,
                                     height: 40,
                                     borderRadius: 5,
                                     justifyContent: 'center',
                                     alignItems: 'center',
-                                    ...style.shadow
+                                    ...style.shadow,
+
                                 }}>
-                                    <Text>MOVER</Text>
-                                </TouchableOpacity>
-                                <TouchableOpacity
-                                    style={{
-                                        backgroundColor: colors.secondary,
-                                        width: 70,
-                                        height: 40,
-                                        borderRadius: 5,
-                                        justifyContent: 'center',
-                                        alignItems: 'center',
-                                        ...style.shadow,
+                                <Text>PARAR</Text>
+                            </TouchableOpacity>
+                        </View>
+                    </ModalPop>
+                </View>
 
-                                    }}>
-                                    <Text>PARAR</Text>
-                                </TouchableOpacity>
-                            </View>
-                        </ModalPop>
-                    </View>
+            </ImageBackground>
+        </View >
 
-                </ImageBackground>
-            </View>
-        </View>
     );
 };
 
@@ -177,38 +248,40 @@ const styles = StyleSheet.create({
         backgroundColor: '#eee'
     },
     image: {
-        flex: 1,
-        resizeMode: "cover",
+        height: '100%',
+        resizeMode: 'cover',
         backgroundColor: '#eee',
         flexDirection: 'column',
-        justifyContent: 'space-between'
-    },
-    mapContainer: {
-        height: '94%',
-        backgroundColor: '#eee'
+        justifyContent: 'space-between',
     },
     topBtnContainer: {
         width: '100%',
         height: '20%',
         flexDirection: 'row',
         justifyContent: 'space-between',
+        top: 15,
+
     },
     btnTop: {
-        width: "40%",
+        width: "30%",
         height: "100%",
     },
     botBtnContainer: {
         width: '100%',
-        height: '25%',
+        height: '15%',
         flexDirection: 'row',
-        justifyContent: 'flex-end',
+        justifyContent: 'space-between',
+        bottom: -130,
+
     },
     btnBot: {
         width: "30%",
         height: "100%",
+        backgroundColor: "#fff",
+
+
     },
     floatingbtn: {
-
         width: 70,
         height: 70,
         borderRadius: 35,
@@ -262,7 +335,7 @@ const style = StyleSheet.create({
     shadow: {
         shadowColor: '#7F5DF0',
         shadowOffset: {
-            with: 0,
+            width: 0,
             height: 10,
         },
         shadowOpacity: 0.25,
